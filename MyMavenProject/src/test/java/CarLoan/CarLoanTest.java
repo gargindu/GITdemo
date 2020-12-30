6f18d7654f660a659791c40d03d29ff48e7a4a5e
indu <garg.indu1@gmail.com>
2020-12-30 18:01:32 -0500

    first commit

---
+++ MyMavenProject/src/test/java/CarLoan/CarLoanTest.java
@@ -0,0 +1,23 @@
+package CarLoan;
+
+import org.testng.annotations.Test;
+
+public class CarLoanTest {
+	
+	
+	
+	@Test
+	public void carLoan()
+	{
+		System.out.println("CAr loan passed");
+		
+	}
+	
+	@Test
+	public void carType()
+	{
+		System.out.println("WE have 3 type of cars");
+		
+	}
+
+}
---
+++ MyMavenProject/src/test/java/HomeLoan/HomeLoanTest.java
@@ -0,0 +1,17 @@
+package HomeLoan;
+
+import org.testng.annotations.Test;
+
+public class HomeLoanTest {
+	
+	
+	
+	@Test
+	public void homeLoan()
+	
+	{
+		System.out.println("home loan passed");
+		
+	}
+
+}
